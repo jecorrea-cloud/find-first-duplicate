f417ff3a5b89c262c64572da67623213a6d9d9be
joaquin <jecorrea@protonmail.com>
2021-07-26 18:36:29 -0400

    Completed assignment

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,5 +1,16 @@
 function findFirstDuplicate(arr) {
   // type your code here
+  let uniques = []
+
+  for (const element of arr){
+    if (uniques.includes(element)){
+      return element
+    }
+    uniques.push(element)
+  }
+
+  return -1
+  
 }
 
 if (require.main === module) {
@@ -11,9 +22,27 @@ if (require.main === module) {
 
   console.log("Expecting: -1");
   console.log("=>", findFirstDuplicate([1, 2, 3, 4]));
+
+
 }
 
 module.exports = findFirstDuplicate;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+/*
+ * initialize an empty array called 'uniques'
+ * 
+ * iterate through the input array:
+ *    if the value is already in uniques, return the value
+ *    else push the value to uniques
+ * 
+ * return -1 if no duplicate found during iteration
+ * *************************************************
+ * I started my solution with an empty array that
+ * would receive each element from the input array.
+ * If I add the condition at first, I will know there 
+ * is an element in the array that is repeated.
+ * Lastly, if the iteration ends and there were no
+ * duplicates, the return value will be -1
+*/
